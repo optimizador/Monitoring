a2c50d7add04539295e6bf2e7e3e3a4db05bfcad
BryanJCCF <83989847+BryanJCCF@users.noreply.github.com>
2021-07-12 20:35:32 -0500

    templates

--- index.js
+++ index.js
@@ -2,30 +2,30 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
 const port = 3000
+
+// obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
+var publicPath = path.resolve(__dirname, 'public'); 
+// Para que los archivos estaticos queden disponibles.
+app.use(express.static(publicPath));
+
+
 
 app.get('/',(request,response)=> {
    
-    response.sendFile(path.resolve(__dirname, './views/index.html'))
+    response.sendFile(path.resolve(__dirname, './index.html'))
 })
 
     app.get('/VATLA',(request,response)=> {
    
-        response.sendFile(path.resolve(__dirname, './views/VATLA.html'))
+        response.sendFile(path.resolve(__dirname, './VATLA.html'))
     })
-    app.get('/VATLA',(request,response)=> {
-   
-        response.sendFile(path.resolve(__dirname, './views/ATLA.js'))
-    })
+    
     app.get('/VLG',(request,response)=> {
    
-        response.sendFile(path.resolve(__dirname, './views/VLG.html'))
+        response.sendFile(path.resolve(__dirname, './VLG.html'))
     })
-    app.get('/VLG',(request,response)=> {
    
-        response.sendFile(path.resolve(__dirname, './views/manager.js'))
-    })
 app.listen(port, () => {
     console.log('La aplicacion esta en linea!');
 })
