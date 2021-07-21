94e527e2322169c3625ae98c0c88aae005253d4c
BryanJCCF <83989847+BryanJCCF@users.noreply.github.com>
2021-07-21 10:41:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 31184
 
 // obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
 var publicPath = path.resolve(__dirname, 'public'); 
